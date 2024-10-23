4f66500ae3716895a77ab1a1e7f834186fd53900
d-abing <ynlio117@naver.com>
2024-10-23 21:15:49 +0900

    <Part4:Ch3:05> Surface

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -1,41 +1,29 @@
 package com.example.fastcampuscompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
-import androidx.compose.ui.unit.sp
 import com.example.fastcampuscompose.ui.theme.FastCampusComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,186 +32,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                ModifierEx()
+                SurfaceEx("Android")
             }
         }
     }
 }
 
 @Composable
-fun ModifierEx() {
-    /*Button(onClick = {}) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        modifier = Modifier.fillMaxSize()
+fun SurfaceEx(name: String) {
+    /*Surface(
+        modifier = Modifier.padding(5.dp)
     ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        modifier = Modifier.height(100.dp)
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        modifier = Modifier
-            .height(100.dp)
-            .width(200.dp)
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        modifier = Modifier.size(200.dp)
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        modifier = Modifier
-            .size(200.dp)
-            .background(Color.Red) // 버튼을 둘러싼 공간의 색상을 변경
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        modifier = Modifier.size(200.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Magenta, // 버튼 배경색이 변경
-            contentColor = Color.Cyan // 버튼 속 콘텐츠의 색이 변경
-        )
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        modifier = Modifier
-            .size(200.dp)
-            .padding(10.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Magenta,
-            contentColor = Color.Cyan
-        )
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
-        Text("Search")
-    }*/
-
-    /*Button(
-        onClick = {},
-        enabled = false,
-        modifier = Modifier
-            .size(200.dp)
-            .padding(10.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Magenta,
-            contentColor = Color.Cyan
-        )
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null
-        )
-        Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
-        )
         Text(
-            text = "Search",
-            modifier = Modifier.clickable {}
+            text = "Hello $name!",
+            modifier = Modifier.padding(8.dp)
         )
     }*/
 
-    Button(
-        onClick = {},
-        modifier = Modifier
-            .size(200.dp)
-            .padding(10.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Magenta,
-            contentColor = Color.Cyan
+    /*Surface(
+        modifier = Modifier.padding(5.dp),
+        shadowElevation = 5.dp
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(8.dp)
+        )
+    }*/
+
+    /*Surface(
+        modifier = Modifier.padding(5.dp),
+        shadowElevation = 5.dp,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.Magenta
         )
     ) {
-        Icon(
-            imageVector = Icons.Filled.Search,
-            contentDescription = null,
-            modifier = Modifier.background(Color.Blue)
-        )
-        Spacer(
-            modifier = Modifier
-                .size(ButtonDefaults.IconSpacing)
-                .background(Color.Blue)
-        )
         Text(
-            text = "Search",
-            modifier = Modifier
-                .offset(x = 20.dp, y = 10.dp)
-                .background(Color.Blue)
+            text = "Hello $name!",
+            modifier = Modifier.padding(8.dp)
+        )
+    }*/
+
+    Surface(
+        modifier = Modifier.padding(5.dp),
+        shadowElevation = 5.dp,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.Magenta
+        ),
+        shape = CircleShape,
+        color = MaterialTheme.colorScheme.error
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(8.dp)
         )
     }
 }
@@ -232,6 +94,6 @@ fun ModifierEx() {
 @Composable
 fun GreetingPreview() {
     FastCampusComposeTheme {
-        ModifierEx()
+        SurfaceEx("Android")
     }
 }
