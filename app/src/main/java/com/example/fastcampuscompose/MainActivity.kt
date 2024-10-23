e1350887c05b33df2751ae5efa24a25c59d0e858
d-abing <ynlio117@naver.com>
2024-10-23 22:06:58 +0900

    <Part4:Ch3:09> BoxWithConstraints

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -4,35 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key.Companion.Ro
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fastcampuscompose.ui.theme.FastCampusComposeTheme
@@ -43,53 +24,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                ColumnEx()
+                Outer()
             }
         }
     }
 }
 
 @Composable
-fun ColumnEx() {
-    /*Column(modifier = Modifier.size(100.dp)) {
-        Text(text = "첫 번째")
-        Text(text = "두 번째")
-        Text(text = "세 번째")
-    }*/
-
-    /*Column(
-        horizontalAlignment = Alignment.End,
-        modifier = Modifier.size(100.dp)
-    ) {
-        Text(text = "첫 번째")
-        Text(text = "두 번째")
-        Text(text = "세 번째")
-    }*/
-
-    /*Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.End,
-        modifier = Modifier.size(100.dp)
-    ) {
-        Text(text = "첫 번째")
-        Text(text = "두 번째")
-        Text(text = "세 번째")
-    }*/
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.End,
-        modifier = Modifier.size(100.dp)
-    ) {
-        Text(
-            text = "첫 번째",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+fun Outer() {
+    Column(modifier = Modifier.width(150.dp)) {
+        Inner(
+            modifier = Modifier
+                .width(200.dp)
+                .height(160.dp)
         )
-        Text(text = "두 번째")
-        Text(
-            text = "세 번째",
-            modifier = Modifier.align(Alignment.Start)
+        Inner(
+            modifier = Modifier
+                .width(200.dp)
+                .height(100.dp)
         )
+    }
+}
+
+@Composable
+private fun Inner(modifier: Modifier = Modifier) {
+    BoxWithConstraints(modifier) {
+        if (maxHeight > 150.dp) {
+            Text(
+                text = "여기 꽤 길군요!",
+                modifier = Modifier.align(Alignment.BottomCenter)
+            )
+        }
+        Text("maxW:$maxWidth maxH:$maxHeight minW:$minWidth minH:$minHeight")
     }
 }
 
@@ -97,6 +63,6 @@ fun ColumnEx() {
 @Composable
 fun GreetingPreview() {
     FastCampusComposeTheme {
-        ColumnEx()
+        Outer()
     }
 }
