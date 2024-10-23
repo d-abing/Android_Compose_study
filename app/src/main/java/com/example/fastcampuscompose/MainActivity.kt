4f46ea6b5e6bd07b882668ee6303b5b3b20a7089
d-abing <ynlio117@naver.com>
2024-10-23 21:46:21 +0900

    <Part4:Ch3:07> Row

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -6,14 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -25,6 +30,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.Ro
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fastcampuscompose.ui.theme.FastCampusComposeTheme
@@ -35,29 +42,95 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                BoxEx()
+                RowEx()
             }
         }
     }
 }
 
 @Composable
-fun BoxEx() {
-    /*Box(modifier = Modifier.size(100.dp)) {
-        Text(text = "Hello World", modifier = Modifier.align(Alignment.BottomEnd))
-        Text(text = "Jetpack", modifier = Modifier.align(Alignment.CenterEnd))
-        Text(text = "Compose", modifier = Modifier.align(Alignment.TopStart))
+fun RowEx() {
+    /*Row(modifier = Modifier.height(40.dp)) {
+        Text(text = "첫 번째!")
+        Text(text = "두 번째!")
+        Text(text = "세 번째!")
     }*/
 
-    /*Box(modifier = Modifier.size(100.dp)) {
-        Box(modifier = Modifier.size(70.dp).background(Color.Cyan).align(Alignment.CenterStart))
-        Box(modifier = Modifier.size(70.dp).background(Color.Yellow).align(Alignment.BottomEnd))
+    /*Row(modifier = Modifier.height(40.dp)) {
+        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.Top))
+        Text(text = "두 번째!", modifier = Modifier.align(Alignment.CenterVertically))
+        Text(text = "세 번째!", modifier = Modifier.align(Alignment.Bottom))
     }*/
 
-    Box {
-        Box(modifier = Modifier.fillMaxSize().background(Color.Cyan).align(Alignment.CenterStart))
-        Box(modifier = Modifier.matchParentSize().background(Color.Green).align(Alignment.CenterStart))
-        Box(modifier = Modifier.size(70.dp).background(Color.Yellow).align(Alignment.Center))
+    /*Row(
+        verticalAlignment = Alignment.Bottom,
+        modifier = Modifier.height(40.dp)
+    ) {
+        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.Top))
+        Text(text = "두 번째!")
+        Text(text = "세 번째!")
+    }*/
+
+    /*Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Bottom,
+        modifier = Modifier
+            .height(40.dp)
+            .width(200.dp)
+    ) {
+        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.Top))
+        Text(text = "두 번째!")
+        Text(text = "세 번째!")
+    }*/
+
+    /*Row(
+        verticalAlignment = Alignment.Bottom,
+        modifier = Modifier
+            .height(40.dp)
+            .width(200.dp)
+    ) {
+        Text(
+            text = "첫 번째!",
+            modifier = Modifier.align(Alignment.Top)
+                .weight(3f)
+        )
+        Text(
+            text = "두 번째!",
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = "세 번째!",
+            modifier = Modifier.weight(3f)
+        )
+    }*/
+
+    Row(
+        verticalAlignment = Alignment.Bottom,
+        modifier = Modifier
+            .height(40.dp)
+            .width(200.dp)
+    ) {
+        Text(
+            text = "첫 번째!",
+            textAlign = TextAlign.End,
+            modifier = Modifier.align(Alignment.Top)
+                .weight(3f)
+                .background(Color.Magenta)
+        )
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = "추가",
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.Cyan)
+        )
+        Text(
+            text = "세 번째!",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .weight(3f)
+                .background(Color.Blue)
+        )
     }
 }
 
@@ -65,6 +138,6 @@ fun BoxEx() {
 @Composable
 fun GreetingPreview() {
     FastCampusComposeTheme {
-        BoxEx()
+        RowEx()
     }
 }
