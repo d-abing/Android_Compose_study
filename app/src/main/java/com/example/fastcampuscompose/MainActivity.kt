e9a99e4a4902a3c63dc8b00580ec39bb98645951
d-abing <ynlio117@naver.com>
2024-10-23 17:45:46 +0900

    <Part4:Ch3:02> Text

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.sp
 import com.example.fastcampuscompose.ui.theme.FastCampusComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +25,70 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    Greeting("Android")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
+    // Text(text = "Hello $name")
+
+    // Text(color = Color.Red, text = "Hello $name")
+
+    // Text(color = Color(0xffff9944), text = "Hello $name")
+
+    // Text(color = Color.Red, text = "Hello $name", fontSize = 30.sp)
+
+    // Text(color = Color.Red, text = "Hello $name", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+
+    /*Text(
+        color = Color.Red,
+        text = "Hello $name",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive
+    )*/
+
+    /*Text(
+        color = Color.Red,
+        text = "Hello $name",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        letterSpacing = 2.sp
+    )*/
+
+    /*Text(
+        color = Color.Red,
+        text = "Hello $name\nHello $name\nHello $name",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        maxLines = 2
+    )*/
+
+    /*Text(
+        color = Color.Red,
+        text = "Hello $name\nHello $name\nHello $name",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        maxLines = 2,
+        textDecoration = TextDecoration.Underline
+    )*/
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier.width(300.dp),
+        color = Color.Red,
+        text = "Hello $name\nHello $name\nHello $name",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        maxLines = 2,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center
     )
 }
 
