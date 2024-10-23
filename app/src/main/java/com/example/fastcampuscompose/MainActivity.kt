9f71b64cd00a1997855417e55242475e984a1f01
d-abing <ynlio117@naver.com>
2024-10-23 22:17:39 +0900

    <Part4:Ch3:10> Image

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.fastcampuscompose.ui.theme.FastCampusComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,38 +19,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                Outer()
+                ImageEx()
             }
         }
     }
 }
 
 @Composable
-fun Outer() {
-    Column(modifier = Modifier.width(150.dp)) {
-        Inner(
-            modifier = Modifier
-                .width(200.dp)
-                .height(160.dp)
+fun ImageEx() {
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.wall),
+            contentDescription = "엔텔로프 캐년"
         )
-        Inner(
-            modifier = Modifier
-                .width(200.dp)
-                .height(100.dp)
-        )
-    }
-}
 
-@Composable
-private fun Inner(modifier: Modifier = Modifier) {
-    BoxWithConstraints(modifier) {
-        if (maxHeight > 150.dp) {
-            Text(
-                text = "여기 꽤 길군요!",
-                modifier = Modifier.align(Alignment.BottomCenter)
-            )
-        }
-        Text("maxW:$maxWidth maxH:$maxHeight minW:$minWidth minH:$minHeight")
+        Image(
+            imageVector = Icons.Filled.Settings,
+            contentDescription = "세팅"
+        )
     }
 }
 
@@ -63,6 +44,6 @@ private fun Inner(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     FastCampusComposeTheme {
-        Outer()
+        ImageEx()
     }
 }
