20010afc38a1f45864e3ba599838b270b854451f
d-abing <ynlio117@naver.com>
2024-10-24 15:42:03 +0900

    <Part4:Ch3:13> Checkbox

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,10 +14,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,76 +40,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        CardEx(cardData)
-                        CardEx(cardData)
-                    }
-                }
+                CheckBoxEx()
             }
         }
-    }
-
-    companion object {
-        val cardData = CardData(
-            imageUri = "https://picsum.photos/100/100",
-            imageDescription = "랜덤 이미지",
-            author = "picsum",
-            description = "Picsum은 랜덤 이미지를 제공합니다."
-        )
     }
 }
 
 @Composable
-fun CardEx(cardData: CardData) {
-    val placeHolderColor = Color(0x33000000)
-
-    Card(
-        elevation = CardDefaults.cardElevation(8.dp),
-        modifier = Modifier.padding(4.dp),
+fun CheckBoxEx() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(end = 12.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(8.dp)
-        ) {
-            /*AsyncImage(
-                model = cardData.imageUri,
-                contentDescription = cardData.imageDescription,
-                modifier = Modifier.size(32.dp)
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            Column {
-                Text(
-                    text = cardData.author
-                )
-                Spacer(modifier = Modifier.size(4.dp))
-                Text(
-                    text = cardData.description
-                )
-            }*/
 
-            AsyncImage(
-                model = cardData.imageUri,
-                contentScale = ContentScale.Crop,
-                contentDescription = cardData.imageDescription,
-                placeholder = ColorPainter(placeHolderColor),
-                modifier = Modifier.size(32.dp)
-                    .clip(CircleShape)
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            Column {
-                Text(
-                    text = cardData.author
-                )
-                Spacer(modifier = Modifier.size(4.dp))
-                Text(
-                    text = cardData.description
-                )
+        /*Checkbox(checked = false, onCheckedChange = {})*/
+
+        /*var checked = false
+        Checkbox(
+            checked = checked,
+            onCheckedChange = {
+                checked = !checked
             }
-        }
+        )*/
+
+        /*val checked = remember { mutableStateOf(false) }
+        Checkbox(
+            checked = checked.value,
+            onCheckedChange = {
+                checked.value = !checked.value
+            }
+        )*/
+
+        /*var checked by remember { mutableStateOf(false) }
+        Checkbox(
+            checked = checked,
+            onCheckedChange = {
+                checked = !checked
+            }
+        )*/
+
+        val (getter, setter) = remember { mutableStateOf(false) }
+        Checkbox(
+            checked = getter,
+            onCheckedChange = setter
+        )
+
+        Text(
+            text = "프로그래머입니까?",
+            modifier = Modifier.clickable {
+                setter(!getter)
+            }
+        )
     }
 }
 
@@ -111,15 +98,6 @@ fun CardEx(cardData: CardData) {
 @Composable
 fun GreetingPreview() {
     FastCampusComposeTheme {
-        Row {
-            CardEx(MainActivity.cardData)
-        }
+        CheckBoxEx()
     }
 }
-
-data class CardData(
-    val imageUri: String,
-    val imageDescription: String,
-    val author: String,
-    val description: String
-)
