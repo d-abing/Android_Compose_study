6caa16254a56b80baaaf2d554e6f98eb73cef8a9
d-abing <ynlio117@naver.com>
2024-10-24 16:00:41 +0900

    <Part4:Ch3:15> TopAppBar

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -12,14 +12,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -42,47 +52,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                TextFieldEx()
+                TopAppBarEx("Android")
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextFieldEx() {
-    var name by remember { mutableStateOf("Tom") }
+fun TopAppBarEx(name: String) {
+    Column {
+        /*TopAppBar(title = { Text("TopAppBar") })*/
 
-    Column(modifier = Modifier.padding(16.dp)) {
-
-        /*TextField(value = "Tom", onValueChange = {})*/
-
-        /*TextField(value = name, onValueChange = {name = it})*/
-
-        /*TextField(
-            value = name,
-            label = {
-                Text("이름")
-            },
-            onValueChange = {name = it}
+        /*TopAppBar(
+            title = { Text("TopAppBar") },
+            navigationIcon = {
+                IconButton(onClick = { *//*TODO*//* }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "업 네비게이션"
+                    )
+                }
+            }
         )*/
 
-        OutlinedTextField(
-            value = name,
-            label = {
-                Text("이름")
+        TopAppBar(
+            title = { Text("TopAppBar") },
+            navigationIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "업 네비게이션"
+                    )
+                }
             },
-            onValueChange = {name = it}
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "검색"
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "설정"
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.AccountBox,
+                        contentDescription = "계정"
+                    )
+                }
+            }
         )
-        Spacer(modifier = Modifier.size(8.dp))
 
-        Text(text = "Hello $name")
+        Text(text = "Hello $name!")
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FastCampusComposeTheme {
-        TextFieldEx()
+        TopAppBarEx("Android")
     }
 }
