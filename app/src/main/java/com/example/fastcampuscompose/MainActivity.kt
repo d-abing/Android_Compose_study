48ba942e449713c70dd0d57c3ecffc657390ca7c
d-abing <ynlio117@naver.com>
2024-10-24 15:24:41 +0900

    <Part4:Ch3:12> 프로필 카드 구현 실습

--- app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
+++ app/src/main/java/com/example/fastcampuscompose/MainActivity.kt
@@ -4,15 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
 import com.example.fastcampuscompose.ui.theme.FastCampusComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,23 +34,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FastCampusComposeTheme {
-                CoilEx()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        CardEx(cardData)
+                        CardEx(cardData)
+                    }
+                }
             }
         }
+    }
+
+    companion object {
+        val cardData = CardData(
+            imageUri = "https://picsum.photos/100/100",
+            imageDescription = "랜덤 이미지",
+            author = "picsum",
+            description = "Picsum은 랜덤 이미지를 제공합니다."
+        )
     }
 }
 
 @Composable
-fun CoilEx() {
+fun CardEx(cardData: CardData) {
+    val placeHolderColor = Color(0x33000000)
 
-    Column {
-        val painter = rememberImagePainter(data = "https://picsum.photos/1500/1500")
-        Image(
-            painter = painter,
-            contentDescription = "랜덤 이미지"
-        )
+    Card(
+        elevation = CardDefaults.cardElevation(8.dp),
+        modifier = Modifier.padding(4.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            /*AsyncImage(
+                model = cardData.imageUri,
+                contentDescription = cardData.imageDescription,
+                modifier = Modifier.size(32.dp)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(
+                    text = cardData.author
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(
+                    text = cardData.description
+                )
+            }*/
 
-        AsyncImage(model = "https://picsum.photos/1500/1500", contentDescription = "랜덤 이미지")
+            AsyncImage(
+                model = cardData.imageUri,
+                contentScale = ContentScale.Crop,
+                contentDescription = cardData.imageDescription,
+                placeholder = ColorPainter(placeHolderColor),
+                modifier = Modifier.size(32.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(
+                    text = cardData.author
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(
+                    text = cardData.description
+                )
+            }
+        }
     }
 }
 
@@ -45,6 +111,15 @@ fun CoilEx() {
 @Composable
 fun GreetingPreview() {
     FastCampusComposeTheme {
-        CoilEx()
+        Row {
+            CardEx(MainActivity.cardData)
+        }
     }
 }
+
+data class CardData(
+    val imageUri: String,
+    val imageDescription: String,
+    val author: String,
+    val description: String
+)
